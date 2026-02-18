a6d1033bd3dadd38b8bb10a8f0bdeda82f8e7a55
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-18 20:34:49 +0100

    Create Matrix Header

---
+++ myCode/Matrix.h
@@ -0,0 +1,20 @@
+/*
+ * Matrix.h
+ *
+ *  Created on: 18 Feb 2026
+ *      Author: mateo
+ */
+
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+
+
+class Matrix {
+public:
+	Matrix();
+	virtual ~Matrix();
+};
+
+
+#endif /* MATRIX_H_ */
