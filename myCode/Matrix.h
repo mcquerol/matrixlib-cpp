f09b8617efc641054f3cff35efc6cb9e1b555f11
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-18 21:00:31 +0100

    Add private attributes

--- myCode/Matrix.h
+++ myCode/Matrix.h
@@ -8,9 +8,16 @@
 #ifndef MATRIX_H_
 #define MATRIX_H_
 
-
+#include <vector>
+#include <stdlib.h>
 
 class Matrix {
+
+private:
+	size_t m_rows;
+	size_t m_cols;
+	std::vector<double> m_data;
+
 public:
 	Matrix();
 	virtual ~Matrix();
