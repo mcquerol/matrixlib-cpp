7b1552e239908efc7d7b49369cb6b6af7724a897
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-18 20:56:18 +0100

    Add empty methods to unit-test

---
+++ myCode/UnitTests.cpp
@@ -0,0 +1,49 @@
+/*
+ * UnitTests.cpp
+ *
+ *  Created on: 18 Feb 2026
+ *      Author: mateo
+ */
+
+#include "UnitTests.h"
+
+UnitTests::UnitTests()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+UnitTests::~UnitTests()
+{
+	// TODO Auto-generated destructor stub
+}
+
+void UnitTests::runAllTests()
+{
+
+}
+
+void UnitTests::testMultiplication()
+{
+
+}
+
+void UnitTests::testDeterminant()
+{
+
+}
+
+void UnitTests::testTranspose()
+{
+
+}
+
+void UnitTests::testDotProduct()
+{
+
+}
+
+void UnitTests::testMinorMatrix()
+{
+
+}
