2a95a3839e94f52b46fb1df14ed89101ef83ca54
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-18 21:24:46 +0100

    implement constrcutor and add default returns

--- myCode/Matrix.cpp
+++ myCode/Matrix.cpp
@@ -6,13 +6,17 @@
  */
 
 #include "Matrix.h"
+#include <iostream>
 
-Matrix::Matrix(size_t rows, size_t cols)
+using namespace std;
+
+Matrix::Matrix(size_t rows, size_t cols) : m_rows(rows), m_cols(cols)
 {
 }
 
 bool Matrix::isSquare() const
 {
+	return false; // default value
 }
 
 void Matrix::checkIndexOutOfBounds(size_t row, size_t col) const
@@ -29,69 +33,86 @@ void Matrix::checkMultiplicationDimensions(Matrix rhs) const
 
 const std::pair<size_t, size_t> Matrix::getDimensions() const
 {
+	return pair(0,0);
 }
 
 
-double& Matrix::at(size_t row, size_t col)
+double& Matrix::set(size_t row, size_t col)
 {
+	return 0.0F;
 }
 
 const double& Matrix::at(size_t row, size_t col) const
 {
+	return 0.0F;
 }
 
 void Matrix::print() const
 {
+	cout << "";
 }
 
 Matrix& Matrix::transpose()
 {
+	return this;
 }
 
 Matrix& Matrix::flipHorizontal()
 {
+	return this;
 }
 
 Matrix& Matrix::flipVertical()
 {
+	return this;
 }
 
 const double& Matrix::determinant() const
 {
+	return 0.0F;
 }
 
 Matrix Matrix::minorMatrix(size_t row, size_t col) const
 {
+	return 0.0F;
 }
 
 double Matrix::dot(Matrix other) const
 {
+	return 0.0F;
 }
 
 Matrix Matrix::operator +(Matrix rhs) const
 {
+	return Matrix(0,0);
 }
 
 Matrix Matrix::operator -(Matrix rhs) const
 {
+	return Matrix(0,0);
 }
 
 Matrix Matrix::operator *(Matrix rhs) const
 {
+	return Matrix(0,0);
 }
 
 Matrix Matrix::operator *(double scalar) const
 {
+	return Matrix(0,0);
 }
 
 Matrix Matrix::operator /(double scalar) const
 {
+	return Matrix(0,0);
 }
 
 bool Matrix::operator ==(Matrix rhs) const
 {
+	return false;
 }
 
 bool Matrix::operator !=(Matrix rhs) const
 {
+	return false;
 }
