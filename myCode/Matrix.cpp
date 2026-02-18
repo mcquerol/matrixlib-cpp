e75e811bbc88126077c9a05b5b10de15c233b41a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-18 21:17:31 +0100

    Autogenerated method implementations

--- myCode/Matrix.cpp
+++ myCode/Matrix.cpp
@@ -7,14 +7,91 @@
 
 #include "Matrix.h"
 
-
-
-Matrix::Matrix() {
-	// TODO Auto-generated constructor stub
-
+Matrix::Matrix(size_t rows, size_t cols)
+{
 }
 
-Matrix::~Matrix() {
-	// TODO Auto-generated destructor stub
+bool Matrix::isSquare() const
+{
 }
 
+void Matrix::checkIndexOutOfBounds(size_t row, size_t col) const
+{
+}
+
+void Matrix::checkAddSubtractDimensions(Matrix rhs) const
+{
+}
+
+void Matrix::checkMultiplicationDimensions(Matrix rhs) const
+{
+}
+
+const std::pair<size_t, size_t> Matrix::getDimensions() const
+{
+}
+
+
+double& Matrix::at(size_t row, size_t col)
+{
+}
+
+const double& Matrix::at(size_t row, size_t col) const
+{
+}
+
+void Matrix::print() const
+{
+}
+
+Matrix& Matrix::transpose()
+{
+}
+
+Matrix& Matrix::flipHorizontal()
+{
+}
+
+Matrix& Matrix::flipVertical()
+{
+}
+
+const double& Matrix::determinant() const
+{
+}
+
+Matrix Matrix::minorMatrix(size_t row, size_t col) const
+{
+}
+
+double Matrix::dot(Matrix other) const
+{
+}
+
+Matrix Matrix::operator +(Matrix rhs) const
+{
+}
+
+Matrix Matrix::operator -(Matrix rhs) const
+{
+}
+
+Matrix Matrix::operator *(Matrix rhs) const
+{
+}
+
+Matrix Matrix::operator *(double scalar) const
+{
+}
+
+Matrix Matrix::operator /(double scalar) const
+{
+}
+
+bool Matrix::operator ==(Matrix rhs) const
+{
+}
+
+bool Matrix::operator !=(Matrix rhs) const
+{
+}
