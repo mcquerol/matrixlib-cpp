f9db809625f4abfde29591095ebf6b62937058ef
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-18 20:35:17 +0100

    Create Matrix cpp

---
+++ myCode/Matrix.cpp
@@ -0,0 +1,20 @@
+/*
+ * Matrix.cpp
+ *
+ *  Created on: 18 Feb 2026
+ *      Author: mateo
+ */
+
+#include "Matrix.h"
+
+
+
+Matrix::Matrix() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Matrix::~Matrix() {
+	// TODO Auto-generated destructor stub
+}
+
