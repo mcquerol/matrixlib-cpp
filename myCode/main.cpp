393bbc65bff099981b9713d23efad64904751743
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-20 23:46:09 +0100

    Implement inequality operator overload

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -15,10 +15,48 @@ int main ()
 {
     // TODO: Add your program code here
 
-	Matrix m(3,5);
-	cout << m;
+	Matrix m(2,2);
+	m(0,0) = 1;
+	m(0,1) = 2;
+	m(1,0) = 3;
+	m(1,1) = 4;
 
-	cout << "matrix-cpp has started." << endl;
+	cout << m << endl;
+	Matrix b(2,2);
+
+	b = m * 5;
+	cout << b << endl;
+
+	Matrix c(2,2);
+	cout << "division" << endl;
+	cout << m / 5 << endl;
+
+	cout << "addition" << endl;
+	cout << m + 2.34 << endl;
+
+	cout << "Subtraction" << endl;
+	cout << m - 123.125 << endl;
+
+	Matrix m1(2, 2);
+	Matrix m2(2, 2);
+
+	m1(0,0) = 1;
+	m1(0,1) = 2;
+	m1(1,0) = 3;
+	m1(1,1) = 4;
+
+	m2(0,0) = 1;
+	m2(0,1) = 2;
+	m2(1,0) = 5;
+	m2(1,1) = 4;
+
+	if (m1 == m2) {
+	    std::cout << "Matrices are equal!" << std::endl;
+	}
+	if (m1 != m2) {
+	    std::cout << "Matrices are not equal!" << std::endl;
+	}
+
 
 	return 0;
 }
